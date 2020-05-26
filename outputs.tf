0fbd6b58ea0f0b6abb37e539cd638ee0c845df5f
David Pujadas <dpujadas@gmail.com>
2020-05-26 19:48:33 +0200

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -19,19 +19,19 @@ output "scaleway_k8s_cluster_beta_wildcard_dns" {
 }
 
 output "scaleway_k8s_cluster_beta_config_file" {
-  value = scaleway_k8s_cluster_beta.this.config_file
+  value = scaleway_k8s_cluster_beta.this.kubeconfig[0].config_file
 }
 
 output "scaleway_k8s_cluster_beta_host" {
-  value = scaleway_k8s_cluster_beta.this.host
+  value = scaleway_k8s_cluster_beta.this.kubeconfig[0].host
 }
 
 output "scaleway_k8s_cluster_beta_cluster_ca_certificate" {
-  value = scaleway_k8s_cluster_beta.this.cluster_ca_certificate
+  value = scaleway_k8s_cluster_beta.this.kubeconfig[0].cluster_ca_certificate
 }
 
 output "scaleway_k8s_cluster_beta_token" {
-  value = scaleway_k8s_cluster_beta.this.token
+  value = scaleway_k8s_cluster_beta.this.kubeconfig[0].token
 }
 
 output "scaleway_k8s_cluster_beta_status" {
